c5fa946dad87ed742ccbdbac79924a2f96e39e19
zj591227045 <zj591227045@gmail.com>
2025-07-21 21:23:05 +0800

    项目：修复vercel构建问题

---
+++ next.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, './src'),
+    };
+    return config;
+  },
+};
+
+module.exports = nextConfig;
